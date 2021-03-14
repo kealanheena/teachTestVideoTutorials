2ebb17136a6f088dc2dc31b37e10a3c44363c372
kealanheena <kealanheena@gmail.com>
2021-03-14 12:37:22 +0000

    Refactor: Remove for each blocks and make the if statement more readable by seperating it

--- src/functions/searchForTutorials.js
+++ src/functions/searchForTutorials.js
@@ -7,32 +7,44 @@
 
 import { mockData } from '../__test__/mock-data/mockData';
 
-function searchForTutorials(keyWords) {
-  const keyWordsArray = keyWords.toUpperCase().split(',');
+function searchForTutorials(keywords) {
+  const keywordsArray = keywords.toUpperCase().split(',');
+  const keywordsObject = {};
+
+  keywordsArray.forEach((keyword) => {
+    keywordsObject[keyword] = true;
+  });
+  
   const searchResult = mockData.filter((video)=> {
-    const videoTagsObject = {};
-    const videoTitleWordsObject = {};
+    const videoTags = video.tags
     const videoTitle = video.videoTitle.toUpperCase();
-    const teacherName = video.teacherName.toUpperCase();
     const videoTitleNoSpecialChars = video.videoTitle.replace(/[^a-zA-Z ]/g, "");
+    const teacherName = video.teacherName.toUpperCase();
     const seperatedVideoTitleWordsArray = videoTitleNoSpecialChars.toUpperCase().split(" ");
 
-    const videoTags = video.tags.map((tag) => {
-      return tag.toUpperCase()
-    });
+    let i;
 
-    videoTags.forEach((tag) => {
-      videoTagsObject[tag] = true;
-    });
-    
-    seperatedVideoTitleWordsArray.forEach((word) => {
-      videoTitleWordsObject[word] = true;
-    });
+    // Checks for mathcing tags
+    for(i = 0; i < videoTags.length; i++) {
+      const tag = videoTags[i];
+      if(keywordsObject[tag.toUpperCase()]) {
+        return video;
+      }
+    }
 
-    for(var i = 0; i < keyWordsArray.length; i++) {
-      const keyWord = keyWordsArray[i];
-      if(videoTitle === keyWord || teacherName === keyWord || videoTitleWordsObject[keyWord] || videoTagsObject[keyWord]) {
-        return [{videoTitle: keyWords}];
+    // Checks for mathcing title words
+    // e.g. Practice: Places will check for both Practice and Places
+    for(i = 0; i < seperatedVideoTitleWordsArray.length; i++) {
+      const titleWord = seperatedVideoTitleWordsArray[i];
+      if(keywordsObject[titleWord.toUpperCase()]) {
+        return video;
+      }
+    }
+
+    // Checks for mathcing title and teacher name
+    for(i = 0; i < keywordsArray.length; i++) {
+      if(keywordsObject[videoTitle] || keywordsObject[teacherName]) {
+        return video;
       }
     }
     return false;
